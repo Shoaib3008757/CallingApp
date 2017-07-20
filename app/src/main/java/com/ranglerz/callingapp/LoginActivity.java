0d865d2d7576b1b7030b9f521983825860a38b78
shoaib3008757 <shoaibanwar.vu@gmail.com>
2017-07-20 14:11:57 +0500

    Initial commit

---
+++ app/src/main/java/com/ranglerz/callingapp/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.ranglerz.callingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_login);
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("callerId", ((TextView) findViewById(R.id.callerId)).getText().toString());
+                intent.putExtra("recipientId", ((TextView) findViewById(R.id.recipientId)).getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/ranglerz/callingapp/MainActivity.java
@@ -0,0 +1,107 @@
+package com.ranglerz.callingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.sinch.android.rtc.PushPair;
+import com.sinch.android.rtc.Sinch;
+import com.sinch.android.rtc.SinchClient;
+import com.sinch.android.rtc.calling.Call;
+import com.sinch.android.rtc.calling.CallClient;
+import com.sinch.android.rtc.calling.CallClientListener;
+import com.sinch.android.rtc.calling.CallListener;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Call call;
+    private TextView callState;
+    private SinchClient sinchClient;
+    private Button button;
+    private String callerId;
+    private String recipientId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Intent intent = getIntent();
+        callerId = intent.getStringExtra("callerId");
+        recipientId = intent.getStringExtra("recipientId");
+
+        sinchClient = Sinch.getSinchClientBuilder()
+                .context(this)
+                .userId(callerId)
+                .applicationKey("f5480496-eb89-4433-8e3c-62a2ee49f36c")
+                .applicationSecret("G2GMyN186E6saAFpbeAckw==")
+                .environmentHost("sandbox.sinch.com")
+                .build();
+
+        sinchClient.setSupportCalling(true);
+        sinchClient.startListeningOnActiveConnection();
+        sinchClient.start();
+
+        sinchClient.getCallClient().addCallClientListener(new SinchCallClientListener());
+
+        button = (Button) findViewById(R.id.button);
+        callState = (TextView) findViewById(R.id.callState);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (call == null) {
+                    call = sinchClient.getCallClient().callUser(recipientId);
+                    call.addCallListener(new SinchCallListener());
+                    button.setText("Hang Up");
+                } else {
+                    call.hangup();
+                }
+            }
+        });
+    }
+
+    private class SinchCallListener implements CallListener {
+        @Override
+        public void onCallEnded(Call endedCall) {
+            call = null;
+            button.setText("Call");
+            callState.setText("");
+            setVolumeControlStream(AudioManager.USE_DEFAULT_STREAM_TYPE);
+        }
+
+        @Override
+        public void onCallEstablished(Call establishedCall) {
+            callState.setText("connected");
+            setVolumeControlStream(AudioManager.STREAM_VOICE_CALL);
+        }
+
+        @Override
+        public void onCallProgressing(Call progressingCall) {
+            callState.setText("ringing");
+        }
+
+        @Override
+        public void onShouldSendPushNotification(Call call, List<PushPair> pushPairs) {}
+    }
+
+    private class SinchCallClientListener implements CallClientListener {
+        @Override
+        public void onIncomingCall(CallClient callClient, Call incomingCall) {
+            call = incomingCall;
+            call.answer();
+            call.addCallListener(new SinchCallListener());
+            button.setText("Hang Up");
+        }
+    }
+}
